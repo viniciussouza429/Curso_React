dadcd6b3b28f59d422edc37b621502432fc85cda
viniciussouza429 <vinicius.souza429@hotmail.com>
2025-08-20 23:11:35 -0300

    Correcao TaskPage

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router";
 import Button from "./Button";
 
@@ -17,10 +17,11 @@ function Task({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 w-full text-white p-2 rounded-md ${
+            className={`bg-slate-400 w-full flex items-center gap-2 text-white p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
           <Button onClick={() => onSeeDatailsClick(task)}>
